677ad76459682a22f0991768daffc3f584d86c69
Larpee <pilar.lopezmaggi@gmail.com>
2017-06-06 18:42:00 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.support.v7.appcompat.R;;
 
 public class MainActivity extends AppCompatActivity {
     @Override
