2c138f2b70198bf4aa928be52990e463bf90cb96
Larpee <pilar.lopezmaggi@gmail.com>
2017-06-06 18:42:11 -0300

    Update NumbersActivity.java

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import com.example.android.miwok_app.R;
 
 import java.util.ArrayList;
 
