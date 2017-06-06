9788ea9c5ecea8e0642fe1ea97a796ab9adb2f2f
Pipineta <pilar.lopezmaggi@gmail.com>
2017-06-06 19:40:36 -0300

    Commit message

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,23 @@
+package com.example.android.miwok;
+
+/**
+ * Created by pilar_000 on 06/06/2017.
+ */
+
+public class Word {
+    private String mMiwokWord;
+    private String mDefaultTranslation;
+
+    public Word(String miwokWord, String defaultTranslation) {
+        mMiwokWord = miwokWord;
+        mDefaultTranslation = defaultTranslation;
+    }
+
+    public String getMiwokWord() {
+        return mMiwokWord;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+}
