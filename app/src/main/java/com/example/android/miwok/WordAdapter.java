329eec36f79aaba67fa9f704b35810a822d6e590
Pipineta <pilar.lopezmaggi@gmail.com>
2017-06-07 09:32:45 -0300

    Commit message

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,43 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pilar_000 on 07/06/2017.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+
+        if (listItem == null) {
+            listItem = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokWord = (TextView) listItem.findViewById(R.id.miwok_word);
+        miwokWord.setText(currentWord.getMiwokWord());
+
+        TextView defalutTranslation = (TextView) listItem.findViewById(R.id.english_word);
+        defalutTranslation.setText(currentWord.getDefaultTranslation());
+
+        return listItem;
+    }
+}
