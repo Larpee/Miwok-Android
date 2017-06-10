9fd90e3bd415b63280af34e2e2db53d0f8c558a7
Pipineta <pilar.lopezmaggi@gmail.com>
2017-06-10 19:57:25 -0300

    Commit message

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,6 +38,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defalutTranslation = (TextView) listItem.findViewById(R.id.default_word);
         defalutTranslation.setText(currentWord.getDefaultTranslation());
+
+        ImageView image = (ImageView) listItem.findViewById(R.id.image);
+        image.setImageResource(currentWord.getImageResourceId());
 
         return listItem;
     }
